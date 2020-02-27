3bec90760b12c1af3012f205edcb8774041c1a5f
Fil <filo.bertele@gmail.com>
2020-02-27 21:56:10 +0100

    Add functioning navbar

---
+++ Images/openPage.js
@@ -0,0 +1,11 @@
+function openPage(pageName) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    // Show the specific tab content
+    document.getElementById(pageName).style.display = "block";
+
+}
