bd77e0708e0623c3b4de9f1d677d6882ed2f8419
Fil <filo.bertele@gmail.com>
2020-03-26 18:30:55 +0100

    Fixed input and variable names in get_metar() and data{}

--- meteo.py
+++ meteo.py
@@ -5,13 +5,13 @@ import sys
 
 def main():
     coords = mapquest(sys.argv[1]) if len(sys.argv) > 1 else (53.079, 8.801)
-    metar = get_metar(coords)
+    names_metar_taf = get_metar(coords)
     surface_links = [chartmetuk(), chartsdwd()]
     sigwx_links = aviationweather()
     data = {'coords': coords,
             'surface_links': surface_links,
             'sigwx_links': sigwx_links,
-            'metar': metar}
+            'names_metar_taf': names_metar_taf}
     page = DjangoHelper(data)
     page.show()
 
