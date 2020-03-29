653be4d6a1dd1ba7199d28780c268e3efbd6a9ab
Fil <filo.bertele@gmail.com>
2020-03-29 17:43:34 +0200

    Add sat images, renamed functions

--- meteo.py
+++ meteo.py
@@ -4,14 +4,16 @@ import sys
 
 
 def main():
-    coords = mapquest(sys.argv[1]) if len(sys.argv) > 1 else (53.079, 8.801)
+    coords = get_coord(sys.argv[1]) if len(sys.argv) > 1 else (53.079, 8.801)
     names_metar_taf = get_metar(coords)
-    surface_links = [chartmetuk(), chartsdwd()]
-    sigwx_links = aviationweather()
+    surface_links = [swc_ukmetoffice(), swc_dwd()]
+    sigwx_links = sigwx_aviationweather()
+    sat_links = sat_aviationweather()
     data = {'coords': coords,
             'surface_links': surface_links,
             'sigwx_links': sigwx_links,
-            'names_metar_taf': names_metar_taf}
+            'names_metar_taf': names_metar_taf,
+            'sat_links': sat_links}
     page = DjangoHelper(data)
     page.show()
 
